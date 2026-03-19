f38524929917040cca30651cc0fe259e7afb9719
Alec Luí <dr.elucidator@gmail.com>
2026-03-19 13:59:42 -0300

    Lógica adicional adicionada

---
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace jogoDosDados.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Jogo de corrida dos dados");
+
+
+
+
+    }
+}
